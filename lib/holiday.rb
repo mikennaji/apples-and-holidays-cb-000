c3e031997d3b777f7adb1a267a7224e544ecbe01
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-30 22:01:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,21 +18,39 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |key, value |
+    value.each do |key1, value1|
+      if key1 === :christmas || key1 === :new_years
+        value1 << supply
+      end
+      puts "#{value1}"
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |key, value |
+    value.each do |key1, value1|
+      if key1 === :memorial_day
+        value1 << supply
+      end
+      puts "#{value1}"
+    end
+  end
 end
+
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
@@ -42,6 +60,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values
+
 
 end
 
@@ -59,12 +79,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  answer =[]
+  holiday_hash.each do |key, value |
+    value.each do |key1, value1|
+      if value1.include?("BBQ")
+        answer << key1
+      end
+      answer
+    end
+  end
 end
-
-
-
-
-
-
-
